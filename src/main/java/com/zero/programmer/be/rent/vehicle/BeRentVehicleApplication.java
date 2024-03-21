9a66317118f8f85b275d6285359918fe1bea7ed1
bobi.rival <bahmadrival@gmail.com>
2024-03-21 10:18:01 +0700

    Init Project

---
+++ src/main/java/com/zero/programmer/be/rent/vehicle/BeRentVehicleApplication.java
@@ -0,0 +1,13 @@
+package com.zero.programmer.be.rent.vehicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeRentVehicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeRentVehicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero/programmer/be/rent/vehicle/BeRentVehicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.programmer.be.rent.vehicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeRentVehicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
